03b87c50b43ddc172933d35835f81761de7f6fe3
UnoYakshi <yellowwanna@gmail.com>
2017-10-23 02:58:58 +0300

    Renamed the DebtCollector to app...

--- modules/auth/views.py
+++ modules/auth/views.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, session, current_app, g
 from .forms import LoginForm, SignUpForm
 
-from DebtCollector import db
+from app import db
 #from database import db
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
