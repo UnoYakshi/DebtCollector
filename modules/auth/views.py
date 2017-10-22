dd594c24bd13de8115e40d428bd9b239912ebaf6
UnoYakshi <yellowwanna@gmail.com>
2017-10-22 20:24:13 +0300

    Added naive login/logout via flask-session...

--- modules/auth/views.py
+++ modules/auth/views.py
@@ -1,20 +1,41 @@
 #!/modules/auth/views.py
 
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, redirect, url_for, request, session
+
 
 from .forms import LoginForm
-#from ..users.forms import NewUserForm, SignupForm
 
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
+
+
 @auth_bp.route('/login', methods=['POST'])
 def login():
     form = LoginForm()
-    return form.login.data + form.password.data
+    if form.validate_on_submit():
+        #user = User.getBy(login=form.login.data)
+        #if not user:
+        #    return redirect(url_for('auth.AuthView:index'))
+
+        if form.password.data == '123': #user.password:
+            #login_user(user)
+            session['logged_in'] = True
+            #identity_changed.send(current_app._get_current_object(), identity=Identity(user.id))
+
+            return redirect(request.args.get('next') or '/')
+        #return redirect(url_for('auth.AuthView:index'))
+    #return redirect(url_for('auth.AuthView:index'))
+    return redirect(url_for('index'))
 
 
 @auth_bp.route('/login', methods=['GET'])
 def index():
     form = LoginForm()
     return render_template('login.html', form=form)
+
+
+@auth_bp.route('/logout', methods=['POST'])
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('index'))
